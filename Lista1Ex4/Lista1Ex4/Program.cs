369bde2a5650d33f9027db03736d28c3b494e398
JoanaFrz <joanaferraz.b14@gmail.com>
2025-05-14 21:41:06 -0300

    Add files via upload

---
+++ Lista1Ex4/Lista1Ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double baze, altura, area;
+            Console.Write("Valor da Base: ");
+            baze = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Valor da Altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+            area = baze * altura;
+            if (area > 100)
+            {
+                Console.WriteLine("Terreno Grande");
+            }
+        }
+    }
+}
