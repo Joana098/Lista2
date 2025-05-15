1c0e5de7f9542b0fcbcc68b1290db6ceff631ffd
JoanaFrz <joanaferraz.b14@gmail.com>
2025-05-14 22:03:52 -0300

    Add files via upload

---
+++ Lista2Ex6/Lista2Ex6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double peso, altura, R;
+            Console.Write("Digite seu peso: ");
+            peso = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite sua altura: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+            R = peso / (altura * altura);
+            if(R < 20)
+            {
+                Console.WriteLine("Abaixo do peso.");
+            }
+            if (R < 25 && R >= 20)
+            {
+                Console.WriteLine("Peso ideal.");
+            }
+            else 
+            {
+                Console.WriteLine("Acima do peso.");
+            }
+        }
+    }
+}
