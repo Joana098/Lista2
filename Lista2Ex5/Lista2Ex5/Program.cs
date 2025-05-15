cb3b0cd0a4cc5740b203f1d0f77a6088b8e9c72b
JoanaFrz <joanaferraz.b14@gmail.com>
2025-05-14 21:48:33 -0300

    Add files via upload

---
+++ Lista2Ex5/Lista2Ex5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double baze, altura, area;
+            Console.Write("Digite o valor da Base: ");
+            baze = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Altura: ");
+            Console.Write("Digite o valor da Altura:");
+            altura = Convert.ToDouble(Console.ReadLine());
+            area = baze * altura;
+            if(area >= 100)
+                {
+                Console.WriteLine("Terreno Grande");
+            }
+            else
+            {
+                Console.WriteLine("Terreno Pequeno");
+            }
+        }
+    }
+}
