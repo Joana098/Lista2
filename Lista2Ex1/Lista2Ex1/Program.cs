c659f5a5d19b35f5ea95913ab5342d8e34c31953
JoanaFrz <joanaferraz.b14@gmail.com>
2025-05-14 21:01:16 -0300

    Add files via upload

---
+++ Lista2Ex1/Lista2Ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double V1;
+            Double V2;
+            Console.Write("Digite o 1° valor: ");
+            V1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o 2° valor: ");
+            V2 = Convert.ToDouble(Console.ReadLine());
+            if (V1 > V2)
+            {
+                Console.WriteLine("O maior valor é {0}", V1);
+            }
+            else
+            {
+                Console.WriteLine("O maior valor é {0}", V2);
+            }
+        }
+    }
+}
